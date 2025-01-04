30bb5bc9444108a202f0f6b1ed4ee213820afe4d
Arkuna <nightalpha23@gmail.com>
2025-01-04 17:07:16 +0800

    🐛 Fixed for ernie

--- async-openai/src/types/chat.rs
+++ async-openai/src/types/chat.rs
@@ -645,6 +645,7 @@ pub struct ChatCompletionStreamOptions {
 #[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum FinishReason {
+    Normal,
     Stop,
     Length,
     ToolCalls,
